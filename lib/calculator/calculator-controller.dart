b67742c03bd4848906b4e7667a63cdde4bf41436
jeanmfdias <jeanmfdias@gmail.com>
2019-11-04 09:01:12 -0300

    change folder and create first widget

---
+++ lib/calculator/calculator-controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+
+class CalculatorController extends BlocBase {
+
+  @override
+  void dispose() {
+
+  }
+
+}
