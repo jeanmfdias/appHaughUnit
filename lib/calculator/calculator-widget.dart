19f399ad1150084f800e51351958ee49f522c14f
jeanmfdias <jeanmfdias@gmail.com>
2019-11-05 07:50:34 -0300

    add calc official and layout improviments

--- lib/calculator/calculator-widget.dart
+++ lib/calculator/calculator-widget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class CalculatorWidget extends StatefulWidget {
   @override
@@ -10,7 +12,9 @@ class _CalculatorWidget extends State<CalculatorWidget> {
   final height = TextEditingController();
   final weight = TextEditingController();
 
-  var _calc = 0;
+  final _formBase = GlobalKey<FormState>();
+
+  var _calc = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,31 +22,82 @@ class _CalculatorWidget extends State<CalculatorWidget> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Unidade Haugh'),
+          backgroundColor: Colors.green,
         ),
 
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
 
-          children: [
-            Text('Digite o peso do ovo em gramas:'),
-            TextField(
-              controller: height,
+        body: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: Form(
+            key: _formBase,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Text(
+                  'Digite o peso do ovo em gramas (g):',
+                  style: TextStyle(fontSize: 16),
+                ),
+                TextFormField(
+                  controller: weight,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) {
+                    if (double.tryParse(value) == null) {
+                      return 'Digite um número valido.';
+                    }
+                    if (value.trim().isEmpty) {
+                      return 'Campo requirido.';
+                    }
+                    return null;
+                  },
+                ),
+                Divider(color: Colors.transparent),
+                Text(
+                  'Digite a altura do ovo em milimetros (mm): ',
+                  style: TextStyle(fontSize: 16),
+                ),
+                TextFormField(
+                  controller: height,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) {
+                    if (double.tryParse(value) == null) {
+                      return 'Digite um número valido';
+                    }
+                    return null;
+                  },
+                ),
+                Divider(color: Colors.transparent),
+                RaisedButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    if (_formBase.currentState.validate()) {
+                      setState(() {
+                        _calc = 30.0 * pow(double.tryParse(weight.text), 0.37) - 100.0;
+                        _calc = 32.0 * _calc / 100.0;
+                        _calc = double.tryParse(height.text) - sqrt(_calc) + 1.9;
+                        _calc = 100.0 * (log(_calc) / ln10);
+                      });
+                    } else {
+                      setState(() {
+                        _calc = 0;
+                      });
+                    }
+                  },
+                  child: Text(
+                    'Calcular',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Divider(),
+                Text(
+                  'Resultado é: ${_calc}',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                )
+              ],
             ),
-            Text('Digite a altura do ovo em milimetros: '),
-            TextField(
-              controller: weight,
-            ),
-            RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _calc = int.tryParse(height.text) + int.tryParse(weight.text);
-                });
-              },
-              child: Text('Calcular')
-            ),
-            Text('Resultado é: ${_calc}')
-          ],
+          )
         ),
 
       ),
